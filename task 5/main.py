a1904a3bb0e5f75d45a5e75fbd4778f1a313d48c
Tom18699 <t.maitino@studenti.unipi.it>
2025-12-09 16:03:15 +0100

    updated task 5

--- task 5/main.py
+++ task 5/main.py
@@ -2,8 +2,8 @@ from etl_processor import generate_dw_files
 
 # Assicurati che il nome del JSON sia esatto
 # Se è nella Task 5, devi mettere "../task 5/track_mood_noduplicati_refactoringids.json"
-JSON_FILE = r"C:\Users\Admin\OneDrive - University of Pisa\Desktop\uni\magistrale\LDS\exam\project\track_mood_noduplicati_refactoringids.json" 
-XML_FILE = "artists_normalized.xml"
+JSON_FILE = "track_mood_noduplicati_refactoringids.json" 
+XML_FILE = "artists_filled.xml"
 
 if __name__ == "__main__":
     generate_dw_files(JSON_FILE, XML_FILE)
